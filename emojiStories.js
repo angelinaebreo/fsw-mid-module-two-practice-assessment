62b0cf9545c50f8dc7430cc10096b41dd160ae3b
Angelina Ebreo <angelinaebreo@gmail.com>
2021-02-23 17:15:16 -0500

    complete practice

---
+++ emojiStories.js
@@ -0,0 +1,53 @@
+const emojis = [
+    '😀','😆','😅','🤣','🙃','😍','😡','🥶','😱','👻',
+    '👾','😻','💋','👅','🧵','👠','🥾','👑','🎓','🐔',
+    '🐧','🦄','🦇','🌲','🎄','🌊','🍓','🍍','🥯','🌭',
+    '🍕','🍣','🥡','🤸','🎯','🧨','🔮','💉','🦠','❤️',
+    '🧡','💛','💚','💙','💜','☢️','🚫','💯','🔱','🎊'
+]
+
+document.addEventListener("DOMContentLoaded", () => {
+  const generateBtn = document.querySelector("#generate-story-btn");
+
+  const description = document.querySelector("#description-input").value;
+  const submitBtn = document.querySelector("#submit-description-btn");
+  const history = document.querySelector("div > ul");
+  const historyTitle = document.querySelector("#history-title");
+  const emojiStory = document.querySelector("#emoji-story");
+  let storyArr = [];
+  let storyNumber = 0;
+
+  generateBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    emojiStory.textContent = "";
+
+    const storyLength = Number(
+      document.querySelector("#story-length-input").value
+    );
+
+    for (i = 0; i < storyLength; i++) {
+      let randomN = Math.floor(Math.random() * Math.floor(emojis.length - 1));
+      let emoji = emojis[randomN];
+      storyArr.push(emoji);
+    }
+
+    emojiStory.textContent = storyArr.join("");
+    storyArr = [];
+  });
+
+  submitBtn.addEventListener("click", () => {
+    event.preventDefault();
+    storyNumber++;
+    if (storyNumber === 1) {
+      historyTitle.textContent = `${storyNumber} Saved Story`;
+    } else {
+      historyTitle.textContent = `${storyNumber} Saved Stories`;
+    }
+
+    const description = document.querySelector("#description-input").value;
+    newLi = document.createElement("li");
+    newLi.textContent = `${emojiStory.textContent}: ${description}`;
+
+    history.appendChild(newLi);
+  });
+});
